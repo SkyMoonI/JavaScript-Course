028b0b79385f08d9bba8feef4364f237ef2f18db
Gokay Iseri <gokay.iseri@outlook.com>
2025-06-15 23:38:53 +0300

    New Naming 4-day
    I am at the Section 9: Data Structures, Modern Operators and Strings

---
+++ 09-Data-Structures-Operators/starter/assignment.js
@@ -0,0 +1,475 @@
+const books = [
+  {
+    title: 'Algorithms',
+    author: ['Robert Sedgewick', 'Kevin Wayne'],
+    publisher: 'Addison-Wesley Professional',
+    publicationDate: '2011-03-24',
+    edition: 4,
+    keywords: [
+      'computer science',
+      'programming',
+      'algorithms',
+      'data structures',
+      'java',
+      'math',
+      'software',
+      'engineering',
+    ],
+    pages: 976,
+    format: 'hardcover',
+    ISBN: '9780321573513',
+    language: 'English',
+    programmingLanguage: 'Java',
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.41,
+        ratingsCount: 1733,
+        reviewsCount: 63,
+        fiveStarRatingCount: 976,
+        oneStarRatingCount: 13,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'Structure and Interpretation of Computer Programs',
+    author: [
+      'Harold Abelson',
+      'Gerald Jay Sussman',
+      'Julie Sussman (Contributor)',
+    ],
+    publisher: 'The MIT Press',
+    publicationDate: '2022-04-12',
+    edition: 2,
+    keywords: [
+      'computer science',
+      'programming',
+      'javascript',
+      'software',
+      'engineering',
+    ],
+    pages: 640,
+    format: 'paperback',
+    ISBN: '9780262543231',
+    language: 'English',
+    programmingLanguage: 'JavaScript',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.36,
+        ratingsCount: 14,
+        reviewsCount: 3,
+        fiveStarRatingCount: 8,
+        oneStarRatingCount: 0,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "Computer Systems: A Programmer's Perspective",
+    author: ['Randal E. Bryant', "David Richard O'Hallaron"],
+    publisher: 'Prentice Hall',
+    publicationDate: '2002-01-01',
+    edition: 1,
+    keywords: [
+      'computer science',
+      'computer systems',
+      'programming',
+      'software',
+      'C',
+      'engineering',
+    ],
+    pages: 978,
+    format: 'hardcover',
+    ISBN: '9780130340740',
+    language: 'English',
+    programmingLanguage: 'C',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 1010,
+        reviewsCount: 57,
+        fiveStarRatingCount: 638,
+        oneStarRatingCount: 16,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'Operating System Concepts',
+    author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
+    publisher: 'John Wiley & Sons',
+    publicationDate: '2004-12-14',
+    edition: 10,
+    keywords: [
+      'computer science',
+      'operating systems',
+      'programming',
+      'software',
+      'C',
+      'Java',
+      'engineering',
+    ],
+    pages: 921,
+    format: 'hardcover',
+    ISBN: '9780471694663',
+    language: 'English',
+    programmingLanguage: 'C, Java',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 3.9,
+        ratingsCount: 2131,
+        reviewsCount: 114,
+        fiveStarRatingCount: 728,
+        oneStarRatingCount: 65,
+      },
+    },
+  },
+  {
+    title: 'Engineering Mathematics',
+    author: ['K.A. Stroud', 'Dexter J. Booth'],
+    publisher: 'Palgrave',
+    publicationDate: '2007-01-01',
+    edition: 14,
+    keywords: ['mathematics', 'engineering'],
+    pages: 1288,
+    format: 'paperback',
+    ISBN: '9781403942463',
+    language: 'English',
+    programmingLanguage: null,
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.35,
+        ratingsCount: 370,
+        reviewsCount: 18,
+        fiveStarRatingCount: 211,
+        oneStarRatingCount: 6,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'The Personal MBA: Master the Art of Business',
+    author: 'Josh Kaufman',
+    publisher: 'Portfolio',
+    publicationDate: '2010-12-30',
+    keywords: ['business'],
+    pages: 416,
+    format: 'hardcover',
+    ISBN: '9781591843528',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.11,
+        ratingsCount: 40119,
+        reviewsCount: 1351,
+        fiveStarRatingCount: 18033,
+        oneStarRatingCount: 1090,
+      },
+    },
+  },
+  {
+    title: 'Crafting Interpreters',
+    author: 'Robert Nystrom',
+    publisher: 'Genever Benning',
+    publicationDate: '2021-07-28',
+    keywords: [
+      'computer science',
+      'compilers',
+      'engineering',
+      'interpreters',
+      'software',
+      'engineering',
+    ],
+    pages: 865,
+    format: 'paperback',
+    ISBN: '9780990582939',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.7,
+        ratingsCount: 253,
+        reviewsCount: 23,
+        fiveStarRatingCount: 193,
+        oneStarRatingCount: 0,
+      },
+    },
+  },
+  {
+    title: 'Deep Work: Rules for Focused Success in a Distracted World',
+    author: 'Cal Newport',
+    publisher: 'Grand Central Publishing',
+    publicationDate: '2016-01-05',
+    edition: 1,
+    keywords: ['work', 'focus', 'personal development', 'business'],
+    pages: 296,
+    format: 'hardcover',
+    ISBN: '9781455586691',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.19,
+        ratingsCount: 144584,
+        reviewsCount: 11598,
+        fiveStarRatingCount: 63405,
+        oneStarRatingCount: 1808,
+      },
+    },
+    highlighted: true,
+  },
+];
+
+const [firstBook, secondBook] = books;
+// console.log(firstBook, secondBook);
+
+const [, , thirdBook] = books;
+// console.log(thirdBook);
+
+const ratings = [
+  ['rating', 4.19],
+  ['ratingsCount', 144584],
+];
+
+const [[, rating], [, ratingsCount]] = ratings;
+// console.log(rating, ratingsCount);
+
+const ratingStars = [63405, 1808];
+
+const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+// console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
+
+const { title, author, ISBN } = books[0];
+// console.log(title, author, ISBN);
+
+const { keywords: tags } = books[0];
+// console.log(tags);
+
+const { language, programmingLanguage = 'unknown' } = books[6];
+// console.log(language, programmingLanguage);
+
+let bookTitle = 'unknown';
+let bookAuthor = 'unknown';
+
+({ title: bookTitle, author: bookAuthor } = books[0]);
+// console.log(bookTitle, bookAuthor);
+
+const {
+  thirdParty: {
+    goodreads: { rating: bookRating },
+  },
+} = books[0];
+// console.log(bookRating);
+
+const printBookInfo = function ({ title, author, year = 'year unknown' }) {
+  console.log(`${title} was written by ${author} in ${year}`);
+};
+
+// printBookInfo({
+//   title: 'Algorithms',
+//   author: 'Robert Sedgewick',
+//   year: '2011',
+// });
+// printBookInfo({ title: 'Algorithms', author: 'Robert Sedgewick' });
+
+const bookAuthors = { ...books[0].author, ...books[1].author };
+// console.log(bookAuthors);
+
+const spellWord = function (string) {
+  console.log(...string);
+};
+
+// spellWord('JavaScript');
+
+const [mainKeyword, ...rest] = books[0].keywords;
+// console.log(mainKeyword, rest);
+
+const { publisher: bookPublisher, ...restOfTheBook } = books[1];
+// console.log(bookPublisher, restOfTheBook);
+
+const printBookAuthorsCount = function (title, ...authors) {
+  console.log(`The book "${title}" has ${authors.length} authors`);
+};
+
+// printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne');
+
+const hasExamplesInJava = function (book) {
+  console.log(book.programmingLanguage === 'Java' || 'no data available');
+};
+
+// hasExamplesInJava(books[1]);
+
+// books.forEach(function (book) {
+//   book.onlineContent && console.log(book.title);
+// });
+
+// books.forEach(function (book) {
+//   book.onlineContent ??
+//     console.log(`"${book.title}" provides no data about its online content`);
+// });
+
+// books.forEach(function (book) {
+//   book.edition ||= 1;
+//   book.edition = book.edition || 1;
+//   console.log(book.edition);
+// });
+
+books.forEach(function (book) {
+  // book: {
+  //   thirdParty: {
+  //     goodreads: {
+  //       rating: bookRating;
+  //     }
+  //   }
+  // }
+
+  // const {
+  //   thirdParty: {
+  //     goodreads: { rating: bookRating },
+  //   },
+  // } = book;
+
+  // book.highlighted = book.highlighted && !(bookRating < 4.2);
+  // book.highlighted &&= !(bookRating < 4.2);
+
+  book.highlighted &&= !(book.thirdParty.goodreads.rating < 4.2);
+  // book.highlighted =
+  //   book.highlighted && !(book.thirdParty.goodreads.rating < 4.2);
+  // console.log(book.highlighted);
+});
+
+let pageSum = 0;
+for (const book of books) pageSum += book.pages;
+// console.log(pageSum);
+
+const allAuthors = [];
+for (const book of books) {
+  // console.log(typeof book.author);
+  if (typeof book.author === 'object') {
+    allAuthors.push(...book.author);
+  } else {
+    allAuthors.push(book.author);
+  }
+}
+// console.log(allAuthors);
+
+// for (const [index, author] of allAuthors.entries()) console.log(`${index + 1}. ${author}`);
+
+const bookData = [
+  ['title', 'Computer Networking: A Top-Down Approach'],
+  ['author', ['James F. Kurose', 'Keith W. Ross']],
+  ['publisher', 'Addison Wesley'],
+];
+
+// Do the rest
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  // ...
+  [bookData[1][0]]: bookData[1][1],
+  [bookData[2][0]]: bookData[2][1],
+};
+// console.log(newBook);
+
+const pages = 880;
+const newBook2 = {
+  title: 'The C Programming Language',
+  author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
+  // ...
+  pages,
+};
+// console.log(newBook2);
+
+const getFirstKeyword = function (book) {
+  console.log(book.keywords?.[0] ?? 'undefined');
+};
+// getFirstKeyword(books[0]);
+// getFirstKeyword(newBook2); // from previous tasks
+
+const entries1 = [];
+// console.log(books[0].thirdParty.goodreads);
+
+for (const goodread of Object.keys(books[0].thirdParty.goodreads)) {
+  entries1.push([goodread]);
+}
+// console.log(entries1);
+
+const entries2 = [];
+for (const [key, value] of Object.values(
+  books[0].thirdParty.goodreads
+).entries()) {
+  entries2.push([key, value]);
+}
+// console.log(entries2);
+
+const entries3 = Object.entries(books[0].thirdParty.goodreads);
+// console.log(entries3);
+
+// const book = {
+//   title: 'Clean Code',
+//   author: 'Robert C. Martin',
+//   pages: 464,
+//   published: 2008,
+// };
+
+// // 🔸 1. Tüm key'leri yazdır (klasik yöntem)
+// for (const key in book) {
+//   console.log(key);
+// }
+
+// // 🔸 2. Tüm key'leri yazdır (modern yöntem)
+// for (const key of Object.keys(book)) {
+//   console.log(key);
+// }
+
+// // 🔸 3. Tüm value'ları yazdır (klasik yöntem)
+// for (const key in book) {
+//   console.log(book[key]);
+// }
+
+// // 🔸 4. Tüm value'ları yazdır (modern yöntem)
+// for (const value of Object.values(book)) {
+//   console.log(value);
+// }
+
+// // 🔸 5. Key-value çiftlerini yazdır (klasik yöntem)
+// for (const key in book) {
+//   console.log(`${key}: ${book[key]}`);
+// }
+
+// // 🔸 6. Key-value çiftlerini yazdır (modern yöntem)
+// for (const [key, value] of Object.entries(book)) {
+//   console.log(`${key}: ${value}`);
+// }
+
+// // ---------------------------------------------
+
+// const openingHours1 = {
+//   mon: { open: 9, close: 17 },
+//   tue: { open: 10, close: 18 },
+//   fri: { open: 8, close: 20 },
+// };
+
+// // 🔸 7. Günleri yazdır (klasik yöntem)
+// for (const day in openingHours1) {
+//   console.log(day);
+// }
+
+// // 🔸 8. Günleri yazdır (modern yöntem)
+// for (const key of Object.keys(openingHours1)) {
+//   console.log(key);
+// }
+
+// // 🔸 9. Açılış saatlerini yazdır (klasik yöntem)
+// for (const day in openingHours1) {
+//   console.log(
+//     `On ${day} we open at ${openingHours1[day].open} and close at ${openingHours1[day].close}`
+//   );
+// }
+
+// // 🔸 10. Açılış saatlerini yazdır (modern yöntem)
+// for (const [day, { open, close }] of Object.entries(openingHours1)) {
+//   console.log(`On ${day} we open at ${open} and close at ${close}`);
+// }
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -22,6 +22,22 @@ const mexicanFoods = new Set([
   'garlic',
 ]);
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -30,18 +46,254 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  // ES6 enhanced object literals
+  openingHours,
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta(ing1, ing2) {
+    console.log(`Here is your declicious pasta with ${ing1} and ${ing2}`);
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
   },
 };
+
+////////////////////////
+// Looping Objects: Object Keys, Values, and Entries
+
+// Property NAMES
+const properties = Object.keys(openingHours);
+// console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+// console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+// console.log(values);
+
+// console.log(openingHours);
+// Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// [key, value]
+for (const [day, { open, close }] of entries) {
+  // console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
+////////////////////////
+
+const users = [{ name: 'jonas', email: 'hello@jonas.io' }];
+// console.log(users[0]?.name ?? 'user array empty');
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours?.[day]?.open ?? 'closed';
+  // console.log(`On ${day}, we open at ${open}`);
+}
+
+// console.log(restaurant.orderPasta?.(0, 1) ?? "method doesn't exist.");
+
+// console.log(restaurant.openingHours);
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1. Create one player array for each team
+// (variables 'players1' and 'players2')
+const [players1, players2] = game.players;
+// console.log(players1, players2);
+
+// 2. The first player in any player array is the goalkeeper
+// and the others are field players. For Bayern Munich (team 1)
+// create one variable ('gk') with the goalkeeper's name,
+// and one array ('fieldPlayers') with all the remaining 10
+// field players
+const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
+
+// 3. Create an array 'allPlayers' containing all players of
+// both teams (22 players)
+const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+
+// 4. During the game, Bayern Munich (team 1) used 3 substitute
+// players. So create a new array ('players1Final') containing
+// all the original team1 players plus 'Thiago', 'Coutinho' and
+// 'Perisic'
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
+
+// 5. Based on the game.odds object, create one variable
+// for each odd (called 'team1', 'draw' and 'team2')
+const { team1, x: draw, team2 } = game.odds;
+// console.log(team1, draw, team2);
+
+// 6. Write a function ('printGoals') that receives an arbitrary
+// number of player names (NOT an array) and prints each of them
+// to the console, along with the number of goals that were scored
+// in total (number of player names passed in)
+const printGoals = function (...playerNames) {
+  console.log(...playerNames);
+
+  const { scored: scoredPlayer } = game;
+  console.log(scoredPlayer);
+
+  playerNames.forEach(function (player) {
+    if (scoredPlayer.includes(player)) {
+      const goals = scoredPlayer.filter(p => p === player).length;
+      console.log(`${player} has goaled ${goals} times.`);
+    } else {
+      console.log(`${player} doesn't have and goal.`);
+    }
+  });
+};
+
+// printGoals(...players1);
+// printGoals(...players2);
+
+// 7. The team with the lower odd is more likely to win.
+// Print to the console which team is more likely to win,
+// WITHOUT using an if/else statement or the ternary operator.
+// console.log(`Most likely to win: ${(team1 < team2 && 'team 1') || 'team 2'}`);
+
+// console.log(
+//   `${(team1 < team2 && game.team1) || game.team2} is more likely to win.`
+// );
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+// 1. Loop over the game.scored array and print each
+// player name to the console, along with the goal number
+// (Example: "Goal 1: Lewandowski")
+
+for (const [index, player] of game.scored.entries()) {
+  // console.log(`Goal ${index + 1}: ${player}`);
+}
+
+// 2. Use a loop to calculate the average odd and log it
+// to the console (We already studied how to calculate averages,
+// you can go check if you don't remember)
+const odds = Object.values(game.odds);
+// console.log(odds);
+let average = 0;
+for (const odd of odds) average += odd;
+average /= odds.length;
+// console.log(average);
+
+// 3. Print the 3 odds to the console, but in a nice formatted way,
+//  exaclty like this:
+//       Odd of victory Bayern Munich: 1.33
+//       Odd of draw: 3.25
+//       Odd of victory Borrussia Dortmund: 6.5
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  // console.log(`Odd of ${teamStr} ${odd}`);
+}
+
+// Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+// BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+//       {
+//         Gnarby: 1,
+//         Hummels: 1,
+//         Lewandowski: 2
+//       }
+///////////////////////////////////////
